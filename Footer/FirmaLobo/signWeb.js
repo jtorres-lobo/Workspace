bfdc69cfa5c97cbe9377f939a33cde427c7689ea
Jose Torres <jtorres@lobocreaciones.com>
2022-12-22 12:19:00 -0300

    update jueves

---
+++ Footer/FirmaLobo/signWeb.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+  const d = new Date();
+  let year = d.getFullYear();
+  const signNode = document.querySelector(".  ");
+  signNode.innerHTML = `<span class="">©${year}     . Todos los derechos reservados. </span><br><span class="" style="font-size: 10px;">E-commerce desarrollado por <a href="https://lobocreaciones.com/" target="_blank" style="cursor: pointer; color: inherit; text-decoration: none;" >Lobo Creaciones.</a></span></p>`;
+};
